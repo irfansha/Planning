02f1bf31679f0eabecebb79aa4a70c3db54e8ed4
irfansha <irfansha536@gmail.com>
2020-06-14 10:08:57 +0200

    Added more example usage for pddl library.

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import pddlpy
 
-domprob = pddlpy.DomainProblem('./examples-pddl/domain-03.pddl', './examples-pddl/problem-03.pddl')
+domprob = pddlpy.DomainProblem('./examples-pddl/blocks/domain.pddl', './examples-pddl/blocks/probBLOCKS-4-0.pddl')
 
 print(domprob.initialstate())
+print(domprob.goals())
+print(list(domprob.operators()))
+print(list(domprob.ground_operator('stack')))
+print(list(domprob.ground_operator('stack'))[0].precondition_pos)
