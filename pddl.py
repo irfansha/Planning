44ab7a74899fc1b280014811ce68910c06d91f6b
irfansha <irfansha536@gmail.com>
2020-09-21 13:59:34 +0200

    Remove main function - not required

--- pddl.py
+++ pddl.py
@@ -211,28 +211,3 @@ class PDDL_Parser:
                 neg.append(predicate[-1])
             else:
                 pos.append(predicate)
-
-# ==========================================
-# Main
-# ==========================================
-if __name__ == '__main__':
-    import sys, pprint
-    domain = sys.argv[1]
-    problem = sys.argv[2]
-    parser = PDDL_Parser()
-    print('----------------------------')
-    pprint.pprint(parser.scan_tokens(domain))
-    print('----------------------------')
-    pprint.pprint(parser.scan_tokens(problem))
-    print('----------------------------')
-    parser.parse_domain(domain)
-    parser.parse_problem(problem)
-    print('Domain name: ' + parser.domain_name)
-    for act in parser.actions:
-        print(act)
-    print('----------------------------')
-    print('Problem name: ' + parser.problem_name)
-    print('Objects: ' + str(parser.objects))
-    print('State: ' + str(parser.state))
-    print('Positive goals: ' + str(parser.positive_goals))
-    print('Negative goals: ' + str(parser.negative_goals))
