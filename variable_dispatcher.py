27ddba23f721ff90d29bfda7b4e46ed56399bed4
irfansha <irfansha536@gmail.com>
2020-09-30 19:58:13 +0200

    Add class variable dispatcher

---
+++ variable_dispatcher.py
@@ -0,0 +1,12 @@
+# Irfansha Shaik, 30.09.2020, Aarhus
+
+class VarDispatcher():
+
+
+  def get_vars(self, n):
+    var_list = list(range(self.next_var, self.next_var+n))
+    self.next_var = self.next_var+n
+    return var_list
+
+  def __init__(self):
+    self.next_var = 1
