cf330f5dca6dacdd1d6fd1cd230d06aec425d309
irfansha <irfansha536@gmail.com>
2020-10-05 16:39:32 +0200

    QBF encoding included.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import sys
 from constraints import Constraints as cs
 from transition_gen import TransitionFunction as tf
 from sat_encoding_gen import SatEncoding as se
+from qr_encoding_gen import QREncoding as qr
 from run_quabs import Quabs as qb
 import plan_tester as pt
 
@@ -36,6 +37,19 @@ if __name__ == '__main__':
     run_qb.parse_quabs_output()
     if run_qb.sat:
       run_qb.extract_plan(sat_encoding.action_vars, constraints_extract.action_vars)
+      run_qb.print_plan()
+      pt.test_plan(run_qb.plan, constraints_extract)
+    else:
+      print('plan not found')
+  elif (encoding == 'QR'):
+    qr_encoding = qr(constraints_extract, tfun, k)
+    qr_encoding.print_encoding_tofile(encoding_file_path)
+    #qr_encoding.print_encoding()
+    run_qb = qb(encoding_file_path, output_file_path, solver_path)
+    run_qb.run()
+    run_qb.parse_quabs_output()
+    if run_qb.sat:
+      run_qb.extract_qr_plan(qr_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, k)
       run_qb.print_plan()
       pt.test_plan(run_qb.plan, constraints_extract)
     else:
