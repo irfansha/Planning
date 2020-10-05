ff7401e04db8fd5cc1221fe017e387dd2bfc078a
irfansha <irfansha536@gmail.com>
2020-10-05 16:40:00 +0200

    Extract function started - XXX not complete yet, a bug remains.

--- run_quabs.py
+++ run_quabs.py
@@ -27,11 +27,51 @@ class Quabs():
     else:
       self.sat = 0
 
+  # XXX not complete yet
+  def extract_qr_plan(self, states, constraints, n, k):
+    for i in range(k):
+      temp_plan = []
+      for action in constraints.action_list:
+        valid_action = 1
+        for pre_neg in action.negative_preconditions:
+          var_index = constraints.state_vars.index(pre_neg)
+          #if states[i][var_index] in self.sol_map:
+          if self.sol_map[states[i][var_index]] == 1:
+            valid_action = 0
+            break
+        for pre_pos in action.positive_preconditions:
+          var_index = constraints.state_vars.index(pre_pos)
+          #if states[i][var_index] in self.sol_map:
+          if self.sol_map[states[i][var_index]] == -1:
+            valid_action = 0
+            break
+        for pos_neg in action.del_effects:
+          var_index = constraints.state_vars.index(pos_neg)
+          #if states[i+1][var_index] in self.sol_map:
+          if self.sol_map[states[i+1][var_index]] == 1:
+            valid_action = 0
+            break
+        for pos_pos in action.add_effects:
+          var_index = constraints.state_vars.index(pos_pos)
+          #if states[i+1][var_index] in self.sol_map:
+          if self.sol_map[states[i+1][var_index]] == -1:
+            valid_action = 0
+            break
+        if (valid_action):
+          if (action.name != 'noop'):
+            temp_plan = (action.name, action.parameters)
+            break
+      if (len(temp_plan) == 0):
+        self.plan.append(('noop',()))
+      else:
+        self.plan.append(temp_plan)
+
   def extract_plan(self, actions, action_vars):
     for step_actions in actions:
       for i in range(len(step_actions)):
         if self.sol_map[step_actions[i]] == 1:
           self.plan.append(action_vars[i])
+
 
   def print_plan(self):
     for action in self.plan:
