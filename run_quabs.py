e8ee5623b72aa65c2fddfbfe42af9685cd53b2be
irfansha <irfansha536@gmail.com>
2020-10-06 10:38:28 +0200

    Completed plan extraction for qbf reachbility encoding. - testing remaining.

--- run_quabs.py
+++ run_quabs.py
@@ -27,57 +27,47 @@ class Quabs():
     else:
       self.sat = 0
 
-  # XXX not complete yet
   def extract_qr_plan(self, states, constraints, n, k):
-    for i in range(k):
-      temp_plan = []
-      add_effects = []
-      del_effects = []
+    current_states = []
+    for i in range(k+1):
+      temp_pos_var = []
+      temp_neg_var = []
       for j in range(n):
-        if (self.sol_map[states[i][j]] != self.sol_map[states[i+1][j]]):
-          if (self.sol_map[states[i][j]] == 1 and self.sol_map[states[i+1][j]] == -1):
-            del_effects.append(constraints.state_vars[j])
-          else:
-            add_effects.append(constraints.state_vars[j])
+        if self.sol_map[states[i][j]] == 1:
+          temp_pos_var.append(constraints.state_vars[j])
+        else:
+          temp_neg_var.append(constraints.state_vars[j])
+      current_states.append([temp_pos_var, temp_neg_var])
+    for i in range(k):
       for action in constraints.action_list:
         valid_action = 1
-        for pre_neg in action.negative_preconditions:
-          var_index = constraints.state_vars.index(pre_neg)
-          #if states[i][var_index] in self.sol_map:
-          if self.sol_map[states[i][var_index]] == 1:
+        current_state = [list(current_states[i][0]), list(current_states[i][1])]
+        # precondition must satisfy:
+        for pos_pre_cond in action.positive_preconditions:
+          if pos_pre_cond not in current_state[0]:
             valid_action = 0
             break
-        for pre_pos in action.positive_preconditions:
-          var_index = constraints.state_vars.index(pre_pos)
-          #if states[i][var_index] in self.sol_map:
-          if self.sol_map[states[i][var_index]] == -1:
+        for neg_pre_cond in action.negative_preconditions:
+          if neg_pre_cond not in current_state[1]:
             valid_action = 0
             break
-        # XXX plan extract not complete:
-        for pos_pos in action.add_effects:
-          var_index = constraints.state_vars.index(pos_pos)
-          #if states[i][var_index] in self.sol_map:
-          if self.sol_map[states[i][var_index]] == -1:
-            valid_action = 0
-            break
-        for pos_neg in action.del_effects:
-          var_index = constraints.state_vars.index(pos_neg)
-          #if states[i][var_index] in self.sol_map:
-          if self.sol_map[states[i][var_index]] == 1:
-            valid_action = 0
-            break
-        for add_effect in add_effects:
-          if add_effect not in action.add_effects:
-            valid_action = 0
-            break
-        for del_effect in del_effects:
-          if del_effect not in action.del_effects:
-            valid_action = 0
-            break
+        # applying action current state:
+        for add_state in action.add_effects:
+          if add_state not in current_state[0]:
+            current_state[0].append(add_state)
+          if add_state in current_state[1]:
+            current_state[1].remove(add_state)
+        for del_state in action.del_effects:
+          if del_state not in current_state[1]:
+            current_state[1].append(del_state)
+          if del_state in current_state[0]:
+            current_state[0].remove(del_state)
+        current_state[0].sort()
+        current_state[1].sort()
+        if (current_state != current_states[i+1]):
+          valid_action = 0
         if (valid_action):
-          temp_plan = (action.name, tuple(action.parameters))
-          self.plan.append(temp_plan)
-          break
+          self.plan.append([action.name, tuple(action.parameters)])
 
   def extract_plan(self, actions, action_vars):
     for step_actions in actions:
