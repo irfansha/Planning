c338096fde309c5305b2813f1b937ff8c77c1458
irfansha <irfansha536@gmail.com>
2020-10-27 12:47:53 +0100

    Now allowing optional arguments with default values.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 
 '''
 Todos:
-  1. Add unit testcases for transition gates generator functions.
+  1. XXX
 '''
 
-import sys
+import argparse
 from constraints import Constraints as cs
 from transition_gen import TransitionFunction as tf
 #from transition_gen_withoutamoalo import TransitionFunction as tf
@@ -18,69 +18,76 @@ import plan_tester as pt
 
 # Main:
 if __name__ == '__main__':
-  #start_time = time.time()
-  domain = sys.argv[1]
-  problem = sys.argv[2]
-  k = int(sys.argv[3])
-  encoding = sys.argv[4]
-  encoding_file_path = sys.argv[5]
-  output_file_path = sys.argv[6]
-  solver_path = sys.argv[7]
+  text = "A tool to encode PDDL (strips) problems to SAT/QBF encodings and compute a plan if exists"
+  parser = argparse.ArgumentParser(description=text)
+  parser.add_argument("-V", "--version", help="show program version", action="store_true")
+  parser.add_argument("-d", help="domain file path")
+  parser.add_argument("-p", help="problem file path")
+  parser.add_argument("-k", help="path length",default = 4)
+  parser.add_argument("-e", help="encoding",default = 'QI')
+  parser.add_argument("--encoding_out", help="output encoding file",default = 'encoding.qcir')
+  parser.add_argument("--solver_out", help="solver output file",default = 'solver_output.txt')
+  parser.add_argument("--solver", help="solver path",default = './quabs')
+  args = parser.parse_args()
+
+
+  if args.version:
+    print("Version 0.7.0")
 
   # Extracting constraints from problem:
-  constraints_extract = cs(domain, problem)
+  constraints_extract = cs(args.d, args.p)
   # Generating transition function:
   tfun = tf(constraints_extract)
 
-  if (encoding == 'SAT'):
-    sat_encoding = se(constraints_extract, tfun, k)
-    sat_encoding.print_encoding_tofile(encoding_file_path)
-    run_qb = qb(encoding_file_path, output_file_path, solver_path)
+  if (args.e == 'SAT'):
+    sat_encoding = se(constraints_extract, tfun, args.k)
+    sat_encoding.print_encoding_tofile(args.encoding_out)
+    run_qb = qb(args.encoding_out, args.solver_out, args.solver)
     run_qb.run()
     run_qb.parse_quabs_output()
     if run_qb.sat:
       #run_qb.extract_plan(sat_encoding.action_vars, constraints_extract.action_vars)
-      run_qb.extract_qr_plan(sat_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, k)
+      run_qb.extract_qr_plan(sat_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, args.k)
       run_qb.print_plan()
       pt.test_plan(run_qb.plan, constraints_extract)
     else:
       print('plan not found')
-  elif (encoding == 'QR'):
-    qr_encoding = qr(constraints_extract, tfun, k)
-    qr_encoding.print_encoding_tofile(encoding_file_path)
+  elif (args.e == 'QR'):
+    qr_encoding = qr(constraints_extract, tfun, args.k)
+    qr_encoding.print_encoding_tofile(args.encoding_out)
 
     #qr_encoding.print_encoding()
-    run_qb = qb(encoding_file_path, output_file_path, solver_path)
+    run_qb = qb(args.encoding_out, args.solver_out, args.solver)
     run_qb.run()
     run_qb.parse_quabs_output()
     if run_qb.sat:
-      run_qb.extract_qr_plan(qr_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, k)
+      run_qb.extract_qr_plan(qr_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, args.k)
       run_qb.print_plan()
       pt.test_plan(run_qb.plan, constraints_extract)
     else:
       print('plan not found')
-  elif (encoding == 'QI'):
-    qi_encoding = qi(constraints_extract, tfun, k)
-    qi_encoding.print_encoding_tofile(encoding_file_path)
-    run_qb = qb(encoding_file_path, output_file_path, solver_path)
+  elif (args.e == 'QI'):
+    qi_encoding = qi(constraints_extract, tfun, args.k)
+    qi_encoding.print_encoding_tofile(args.encoding_out)
+    run_qb = qb(args.encoding_out, args.solver_out, args.solver)
     run_qb.run()
     run_qb.parse_quabs_output()
     if run_qb.sat:
-      run_qb.extract_qr_plan(qi_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, k)
+      run_qb.extract_qr_plan(qi_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, args.k)
       run_qb.print_plan()
       pt.test_plan(run_qb.plan, constraints_extract)
     else:
       print('plan not found')
-  elif (encoding == 'CTE'):
-    ct_encoding = cte(constraints_extract, tfun, k)
-    ct_encoding.print_encoding_tofile(encoding_file_path)
-    run_qb = qb(encoding_file_path, output_file_path, solver_path)
+  elif (args.e == 'CTE'):
+    ct_encoding = cte(constraints_extract, tfun, args.k)
+    ct_encoding.print_encoding_tofile(args.encoding_out)
+    run_qb = qb(args.encoding_out, args.solver_out, args.solver)
     run_qb.run()
     run_qb.parse_quabs_output()
     if run_qb.sat:
       print("plan found")
       '''
-      run_qb.extract_qr_plan(qi_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, k)
+      run_qb.extract_qr_plan(qi_encoding.states_gen.states, constraints_extract, tfun.num_state_vars, args.k)
       run_qb.print_plan()
       pt.test_plan(run_qb.plan, constraints_extract)
       '''
