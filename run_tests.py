c900596f8e5869d007623a98506d21ff80dbc744
irfansha <irfansha536@gmail.com>
2021-01-05 12:48:04 +0100

    Updated runtests with new testcases.

--- run_tests.py
+++ run_tests.py
@@ -1,37 +1,38 @@
 import os
 
 
-sat_list = [("./testcases/IPC1/gripper/domain.pddl", "./testcases/IPC1/gripper/prob01.pddl", 11),
-            ("./testcases/IPC1/movie/domain.pddl", "./testcases/IPC1/movie/prob01.pddl", 7),
-            ("./testcases/IPC1/movie/domain.pddl", "./testcases/IPC1/movie/prob02.pddl", 7),
-            ("./testcases/IPC2/Blocks/domain.pddl", "./testcases/IPC2/Blocks/prob01.pddl", 6),
-            ("./testcases/IPC2/Blocks/domain.pddl", "./testcases/IPC2/Blocks/prob02.pddl",12),
-            ("./testcases/IPC2/Elevator/domain.pddl", "./testcases/IPC2/Elevator/prob01.pddl", 4),
-            ("./testcases/IPC2/Elevator/domain.pddl", "./testcases/IPC2/Elevator/prob02.pddl", 10),
-            ("./testcases/IPC3/DriverLog/domain.pddl", "./testcases/IPC3/DriverLog/prob01.pddl", 7),
-            ("./testcases/IPC3/ZenoTravel/domain.pddl", "./testcases/IPC3/ZenoTravel/prob01.pddl", 1),
-            ("./testcases/IPC3/ZenoTravel/domain.pddl", "./testcases/IPC3/ZenoTravel/prob02.pddl", 6),
-            ("./testcases/IPC4/Satellite/domain.pddl", "./testcases/IPC4/Satellite/prob01.pddl", 9),
-            ("./testcases/IPC4/Satellite/domain.pddl", "./testcases/IPC4/Satellite/prob02.pddl", 13),
-            ("./testcases/IPC5/Rovers/domain.pddl", "./testcases/IPC5/Rovers/prob01.pddl", 10),
-            ("./testcases/IPC5/Rovers/domain.pddl", "./testcases/IPC5/Rovers/prob02.pddl", 8)
-            ]
+competition_testcase_path = "./testcases/competition/"
 
-unsat_list = [("./testcases/IPC1/gripper/domain.pddl", "./testcases/IPC1/gripper/prob01.pddl", 10),
-            ("./testcases/IPC1/movie/domain.pddl", "./testcases/IPC1/movie/prob01.pddl", 6),
-            ("./testcases/IPC1/movie/domain.pddl", "./testcases/IPC1/movie/prob02.pddl", 6),
-            ("./testcases/IPC2/Blocks/domain.pddl", "./testcases/IPC2/Blocks/prob01.pddl", 5),
-            ("./testcases/IPC2/Blocks/domain.pddl", "./testcases/IPC2/Blocks/prob02.pddl",11),
-            ("./testcases/IPC2/Elevator/domain.pddl", "./testcases/IPC2/Elevator/prob01.pddl", 3),
-            ("./testcases/IPC2/Elevator/domain.pddl", "./testcases/IPC2/Elevator/prob02.pddl", 9),
-            ("./testcases/IPC3/DriverLog/domain.pddl", "./testcases/IPC3/DriverLog/prob01.pddl", 6),
-            ("./testcases/IPC3/ZenoTravel/domain.pddl", "./testcases/IPC3/ZenoTravel/prob01.pddl", 0),
-            ("./testcases/IPC3/ZenoTravel/domain.pddl", "./testcases/IPC3/ZenoTravel/prob02.pddl", 5),
-            ("./testcases/IPC4/Satellite/domain.pddl", "./testcases/IPC4/Satellite/prob01.pddl", 8),
-            ("./testcases/IPC4/Satellite/domain.pddl", "./testcases/IPC4/Satellite/prob02.pddl", 12),
-            ("./testcases/IPC5/Rovers/domain.pddl", "./testcases/IPC5/Rovers/prob01.pddl", 9),
-            ("./testcases/IPC5/Rovers/domain.pddl", "./testcases/IPC5/Rovers/prob02.pddl", 7)
-            ]
+competition_sat_list = [("IPC1/gripper/", "prob01.pddl", 11), ("IPC1/movie/", "prob01.pddl", 7),
+                        ("IPC1/movie/", "prob02.pddl", 7),
+                        ("IPC2/Blocks/", "prob01.pddl", 6), ("IPC2/Blocks/", "prob02.pddl",12),
+                        ("IPC2/Elevator/", "prob01.pddl", 4), ("IPC2/Elevator/", "prob02.pddl", 10),
+                        ("IPC3/DriverLog/", "prob01.pddl", 7), ("IPC3/ZenoTravel/", "prob01.pddl", 1),
+                        ("IPC3/ZenoTravel/", "prob02.pddl", 6),
+                        ("IPC4/Satellite/", "prob01.pddl", 9), ("IPC4/Satellite/", "prob02.pddl", 13),
+                        ("IPC5/Rovers/", "prob01.pddl", 10), ("IPC5/Rovers/", "prob02.pddl", 8)]
+
+competition_unsat_list = [("IPC1/gripper/", "prob01.pddl", 10), ("IPC1/movie/", "prob01.pddl", 6),
+                          ("IPC1/movie/", "prob02.pddl", 6),
+                          ("IPC2/Blocks/", "prob01.pddl", 5), ("IPC2/Blocks/", "prob02.pddl",11),
+                          ("IPC2/Elevator/", "prob01.pddl", 3), ("IPC2/Elevator/", "prob02.pddl", 9),
+                          ("IPC3/DriverLog/", "prob01.pddl", 6), ("IPC3/ZenoTravel/", "prob01.pddl", 0), ("IPC3/ZenoTravel/", "prob02.pddl", 5),
+                          ("IPC4/Satellite/", "prob01.pddl", 8), ("IPC4/Satellite/", "prob02.pddl", 12),
+                          ("IPC5/Rovers/", "prob01.pddl", 9), ("IPC5/Rovers/", "prob02.pddl", 7)]
+
+classical_testcase_path = "./testcases/classical/"
+
+classical_sat_list = [("ipc1998/untyped/gripper/", "prob01.pddl",11), ("ipc1998/untyped/movie/", "prob01.pddl",7),
+                      #("ipc1998/untyped/mystery/", "prob01.pddl", 5),
+                      ("ipc2000/typed/elevators-00-strips/", "prob01.pddl", 4), ("ipc2000/typed/elevators-00-strips/", "prob02.pddl", 7),
+                      ("ipc2000/untyped/blocks/", "prob01.pddl", 6),
+                      ("ipc2000/untyped/blocks-3op/", "prob01.pddl", 0),
+                      #("ipc2000/untyped/logistics00/", "prob01.pddl", 20)
+                      ("ipc2002/untyped/depot/", "prob01.pddl", 10),
+                      ("ipc2002/untyped/driverlog/", "prob01.pddl", 7),
+                      #("ipc2002/untyped/rovers-02/", "prob01.pddl", 11),
+                      ("ipc2002/untyped/zenotravel/", "prob01.pddl", 1)]
+
 
 
 def gen_new_arguments(domain, problem, k, args):
@@ -63,13 +64,15 @@ def run_tests(args):
     count = 0
     all_success = 1
     # Running testcases that have a plan:
-    for testcase in sat_list:
+    for testcase in competition_sat_list:
       count += 1
       print("\n--------------------------------------------------------------------------------")
       print("testcase" + str(count) + " :")
-      print(testcase)
+      domain = competition_testcase_path + testcase[0] + 'domain.pddl'
+      problem = competition_testcase_path + testcase[0] + testcase[1]
+      print(domain, problem, testcase[2])
       # domain and problem files (and k) are new:
-      command_arguments = gen_new_arguments(testcase[0], testcase[1], testcase[2], args)
+      command_arguments = gen_new_arguments(domain, problem, testcase[2], args)
       # Running testcase and generating plan (if available):
       command = 'python3 main.py ' + command_arguments
       plan_status = os.popen(command).read()
@@ -85,7 +88,46 @@ def run_tests(args):
       elif ('Plan found' in plan_status):
         # Validating the plan generated:
         Val_path = './tools/Validate'
-        command = Val_path + ' ' + testcase[0] + ' ' + testcase[1] + ' ' + args.plan_out
+        command = Val_path + ' ' + domain + ' ' + problem + ' ' + args.plan_out
+        testing_status = os.popen(command).read()
+        if ("Plan valid" not in testing_status):
+          print("failed")
+          # plan failed:
+          all_success = 0
+          exit()
+        else:
+          print("success")
+      else:
+          print("failed! plan must exist")
+          # plan failed:
+          all_success = 0
+      print("--------------------------------------------------------------------------------\n")
+    # Running testcases that have a plan:
+    for testcase in classical_sat_list:
+      count += 1
+      print("\n--------------------------------------------------------------------------------")
+      print("testcase" + str(count) + " :")
+      domain = classical_testcase_path + testcase[0] + 'domain.pddl'
+      problem = classical_testcase_path + testcase[0] + testcase[1]
+      print(domain, problem, testcase[2])
+      # domain and problem files (and k) are new:
+      command_arguments = gen_new_arguments(domain, problem, testcase[2], args)
+      # Running testcase and generating plan (if available):
+      command = 'python3 main.py ' + command_arguments
+      plan_status = os.popen(command).read()
+      print(plan_status)
+      if (args.run == 1):
+        print("Testing only existence")
+        if ('Plan found' in plan_status):
+          print("success")
+        else:
+          # plan failed:
+          all_success = 0
+          print("failed! plan must exist")
+      elif ('Plan found' in plan_status):
+        # Validating the plan generated:
+        Val_path = './tools/Validate'
+        command = Val_path + ' ' + domain + ' ' + problem + ' ' + args.plan_out
         testing_status = os.popen(command).read()
         if ("Plan valid" not in testing_status):
           print("failed")
@@ -101,13 +143,15 @@ def run_tests(args):
       print("--------------------------------------------------------------------------------\n")
 
     # Running unsat testcases:
-    for testcase in unsat_list:
+    for testcase in competition_unsat_list:
       count += 1
       print("\n--------------------------------------------------------------------------------")
       print("testcase" + str(count) + " :")
-      print(testcase)
-      # domain and problem files are new:
-      command_arguments = gen_new_arguments(testcase[0], testcase[1], testcase[2], args)
+      domain = competition_testcase_path + testcase[0] + 'domain.pddl'
+      problem = competition_testcase_path + testcase[0] + testcase[1]
+      print(domain, problem, testcase[2])
+      # domain and problem files (and k) are new:
+      command_arguments = gen_new_arguments(domain, problem, testcase[2], args)
       # Running testcase and generating plan (if available):
       command = 'python3 main.py ' + command_arguments
       plan_status = os.popen(command).read()
