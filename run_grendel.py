4bcee6b6c1ac2df472583e14226f334663c17e34
irfansha <irfansha536@gmail.com>
2021-02-04 10:29:59 +0100

    making output directory explicit.

--- run_grendel.py
+++ run_grendel.py
@@ -1,6 +1,7 @@
 # Irfansha Shaik, 24.01.2021, Aarhus.
 
 import os
+from pathlib import Path
 import argparse
 
 # Main:
@@ -14,6 +15,7 @@ if __name__ == '__main__':
   parser.add_argument("--mail_type", help="mail type", default = 'END')
   parser.add_argument("--mail_user", help="mail", default = 'irfansha.shaik@cs.au.dk')
   parser.add_argument("--only_testing", type =int, help=" only for testing purposes", default = 0)
+  parser.add_argument("--output_dir", help="directory path for output files", default = 'out_data/')
   args = parser.parse_args()
 
   if (args.only_testing == 0):
@@ -28,7 +30,11 @@ if __name__ == '__main__':
 
   encoding_variants = ["UG", "UG_po", "UG_po_pre", "SAT"]
 
-
+  # Checking if out directory exits:
+  if not Path(args.output_dir).is_dir():
+    print("Invalid directory path: " + args.output_dir)
+    print("Creating new directory with same path.")
+    os.mkdir(args.output_dir)
 
   for encoding in encoding_variants:
     for domain in typed_domains:
@@ -61,23 +67,20 @@ if __name__ == '__main__':
 
       f.write("cd $SLURM_SUBMIT_DIR\n\n")
 
-      f.write("CUR_DATE=`date +%Y%m%d%T`\n")
-      f.write('mkdir “$CUR_DATE”\n\n')
-
       default_file_names = ' --encoding_out /scratch/$SLURM_JOB_ID/encoding_$SLURM_JOB_ID --solver_out /scratch/$SLURM_JOB_ID/solver_out_$SLURM_JOB_ID --preprocessed_encoding_out /scratch/$SLURM_JOB_ID/preprocessed_$SLURM_JOB_ID --plan_out /scratch/$SLURM_JOB_ID/plan_$SLURM_JOB_ID --encoding_intermediate_out /scratch/$SLURM_JOB_ID/intermediate_$SLURM_JOB_ID '
 
 
       if(encoding == 'UG'):
-        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --run_benchmarks 1 --time_limit 5000 > “$CUR_DATE”/out_UG_" + domain_name + "_$SLURM_JOB_ID\n")
+        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --run_benchmarks 1 --time_limit 5000 > " + args.output_dir + "out_UG_" + domain_name + "_$SLURM_JOB_ID\n")
         command = 'sbatch ' + "run_UG_"+ domain_name + ".sh"
       elif(encoding == 'UG_po'):
-        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --parameters_overlap 1 --run_benchmarks 1 --time_limit 5000 > “$CUR_DATE”/out_UG_po_" + domain_name + "_$SLURM_JOB_ID\n")
+        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --parameters_overlap 1 --run_benchmarks 1 --time_limit 5000 > " + args.output_dir + "out_UG_po_" + domain_name + "_$SLURM_JOB_ID\n")
         command = 'sbatch ' + "run_UG_po_"+ domain_name + ".sh"
       elif(encoding == 'UG_po_pre'):
-        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --preprocessing 1 --run 1 --parameters_overlap 1 --run_benchmarks 1 --time_limit 5000 > “$CUR_DATE”/out_UG_po_pre_" + domain_name + "_$SLURM_JOB_ID\n")
+        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --preprocessing 1 --run 1 --parameters_overlap 1 --run_benchmarks 1 --time_limit 5000 > " + args.output_dir + "out_UG_po_pre_" + domain_name + "_$SLURM_JOB_ID\n")
         command = 'sbatch ' + "run_UG_po_pre_"+ domain_name + ".sh"
       elif(encoding == 'SAT'):
-        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --run_benchmarks 1 -e SAT --solver_type 5 --time_limit 5000 > “$CUR_DATE”/out_SAT_" + domain_name + "$SLURM_JOB_ID\n")
+        f.write("time python3 main.py --dir " + typed_domain_path + domain + default_file_names + " --run_benchmarks 1 -e SAT --solver_type 5 --time_limit 5000 > " + args.output_dir + "out_SAT_" + domain_name + "$SLURM_JOB_ID\n")
         command = 'sbatch ' + "run_SAT_"+ domain_name + ".sh"
 
       f.write("\necho '========= Job finished at `date` =========='\n")
